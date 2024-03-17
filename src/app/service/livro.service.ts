001cc160fc33877905b62d9db5e862ac1da3d42f
lucasdav <lucassouzadavanso@gmail.com>
2024-03-17 18:44:56 -0300

    transformando com o map

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
-import { LivrosResultado } from '../models/interfaces';
+import { Observable, map, tap } from 'rxjs';
+import { Item, LivrosResultado } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -16,10 +16,12 @@ export class LivroService {
   // passará o fluxo de informações para aplicarmos as transformações
   // passaremos o segundo operador chamado tap(), que por sua vez é como se fosse um "espião", 
   // ou seja, é utilizado apenas para debug da aplicação
-  buscar(valorDigitado: string): Observable<LivrosResultado> {
+  buscar(valorDigitado: string): Observable<Item[]> {
     const params = new HttpParams().append('q', valorDigitado);
     return this.http.get<LivrosResultado>(this.API, { params }).pipe(
-      tap((retornoAPI) => console.log('LivroService: ',retornoAPI))
+      tap(retornoAPI => console.log('LivroService: ', retornoAPI)),
+      map(resultado => resultado.items),
+      tap(resultado => console.log('LivroService fluxo após o map: ', resultado)),
     )
   }
 }
