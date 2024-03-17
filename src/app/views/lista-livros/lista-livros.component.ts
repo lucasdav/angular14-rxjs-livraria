64938eb02a5fc15fdafd232db5c1cdd379d0e9f8
lucasdav <lucassouzadavanso@gmail.com>
2024-03-17 18:23:25 -0300

    adicionando o unsubscribe

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -6,10 +7,11 @@ import { LivroService } from 'src/app/service/livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
 
   listaLivros: [];
   campoBusca = '';
+  subscription: Subscription;
 
   constructor(private service: LivroService) { }
 
@@ -17,11 +19,15 @@ export class ListaLivrosComponent {
     //Através da subscription, que representa a execução de um Observable, é possível conectar observer e observable.
     //O Observer é uma coleção de callbacks que sabe escutar os valores entregues pelo Observable.
     //para executar o observable é preciso chamar o subscribe
-    this.service.buscar(this.campoBusca).subscribe({
+    this.subscription = this.service.buscar(this.campoBusca).subscribe({
       next: retornoAPI => console.log(retornoAPI),
       error: erro => console.error(erro),
       complete: () => console.log('Observable completado')
     })
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
   }
 
 }
