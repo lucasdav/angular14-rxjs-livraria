8c0d78933f2ebcf228633ec759dce1e7c2e1aac1
lucasdav <lucassouzadavanso@gmail.com>
2024-03-19 21:33:49 -0300

    catchError e throwError

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { debounceTime, filter, map, switchMap, tap } from 'rxjs';
+import { catchError, debounceTime, distinctUntilChanged, filter, map, switchMap, tap, throwError } from 'rxjs';
 import { Item } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
@@ -28,10 +28,19 @@ export class ListaLivrosComponent {
       debounceTime(PAUSA),
       filter((valorDigitado) => valorDigitado.length >= 3),
       tap(() => console.log('ListaLivrosComponent: fluxo inicial da busca')),
+      // abaixo uso do distinctUntilChanged compara o valor atual com o valor imediatamente anterior 
+      // e não com valores passados já informados. forem iguais, nada acontece. Se forem diferentes, 
+      // é feita uma nova requisição e o valor muda.
+      distinctUntilChanged(),
       // uso do switchMap para performance de busca, ele espera formar a palavra para buscar
       switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
       tap(() => console.log('ListaLivrosComponent: fluxo da requisicao ao servidor para a busca')),
-      map(items => this.livrosResultadoParaLivros(items))
+      map(items => this.livrosResultadoParaLivros(items)),
+      // o operador catchError abaixo captura um erro, se houver
+      catchError(erro => {
+        console.log(erro)
+        return throwError(() => new Error('Ops ocorreu um erro'))
+      })
     )
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
