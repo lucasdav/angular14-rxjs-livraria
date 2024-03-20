c89598e8dc7a8dd13523e2eb05a84a3c89b7f206
lucasdav <lucassouzadavanso@gmail.com>
2024-03-19 21:59:15 -0300

    mostrando total de itens encontrados

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -16,12 +16,14 @@ export class LivroService {
   // passará o fluxo de informações para aplicarmos as transformações
   // passaremos o segundo operador chamado tap(), que por sua vez é como se fosse um "espião", 
   // ou seja, é utilizado apenas para debug da aplicação
-  buscar(valorDigitado: string): Observable<Item[]> {
+  buscar(valorDigitado: string): Observable<LivrosResultado> {
     const params = new HttpParams().append('q', valorDigitado);
-    return this.http.get<LivrosResultado>(this.API, { params }).pipe(
-      tap(retornoAPI => console.log('LivroService: ', retornoAPI)),
-      map(resultado => resultado.items),
-      tap(resultado => console.log('LivroService fluxo após o map: ', resultado)),
-    )
+    return this.http.get<LivrosResultado>(this.API, { params })
+    //.pipe(
+      //tap(retornoAPI => console.log('LivroService: ', retornoAPI)),
+      // abaixo uso do if ternário para se não encontrar nenhum retorno, deve apresentar um objeto vazio
+      //map(resultado => resultado.items ?? []),
+      //tap(resultado => console.log('LivroService fluxo após o map: ', resultado)),
+    //)
   }
 }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { EMPTY, catchError, debounceTime, distinctUntilChanged, filter, map, switchMap, tap, throwError } from 'rxjs';
-import { Item } from 'src/app/models/interfaces';
+import { EMPTY, catchError, debounceTime, distinctUntilChanged, filter, map, of, switchMap, tap, throwError } from 'rxjs';
+import { Item, LivrosResultado } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
@@ -16,8 +16,27 @@ export class ListaLivrosComponent {
 
   campoBusca = new FormControl();
   mensagemErro = '';
+  livrosResultado: LivrosResultado;
 
   constructor(private service: LivroService) { }
+
+  totalDeLivros$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    tap(() => console.log('ListaLivrosComponent: fluxo inicial da busca')),
+    // abaixo uso do distinctUntilChanged compara o valor atual com o valor imediatamente anterior 
+    // e não com valores passados já informados. forem iguais, nada acontece. Se forem diferentes, 
+    // é feita uma nova requisição e o valor muda.
+    distinctUntilChanged(),
+    // uso do switchMap para performance de busca, ele espera formar a palavra para buscar
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    map(resultado => this.livrosResultado = resultado),
+    catchError(erro => {
+      console.log(erro)
+      // abaixo a função dele será emitir um valor e, logo em seguida, completar o Observable
+      return of()
+    })
+  )
 
   // Abaixo o $ no final da variavel indica que é um observable, por convensão de nomenclatura.
   //Através da subscription, que representa a execução de um Observable, é possível conectar observer
@@ -36,15 +55,17 @@ export class ListaLivrosComponent {
       // uso do switchMap para performance de busca, ele espera formar a palavra para buscar
       switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
       tap(() => console.log('ListaLivrosComponent: fluxo da requisicao ao servidor para a busca')),
+      map(resultado => resultado.items ?? []),
       map(items => this.livrosResultadoParaLivros(items)),
       // o operador catchError abaixo captura um erro, se houver
-      catchError(() => {
+      catchError((erro) => {
         this.mensagemErro = 'Ops ocorreu um erro. Recarregue a aplicação';
-        //abaixo EMPTY encerra o ciclo de vida do observable, por isso é necessario recarregar
-        return EMPTY;
-        // console.log(erro)
+        //abaixo EMPTY usado quando não é preciso tratar o erro, ele
+        // encerra o ciclo de vida do observable, por isso é necessario recarregar
+        //return EMPTY;
+        //console.log(erro)
         // abaixo pode ser usado quando precisa tratar erro
-        // return throwError(() => new Error(this.mensagemErro = 'Ops ocorreu um erro. Recarregue a aplicação';))
+        return throwError(() => new Error(this.mensagemErro = 'Ops ocorreu um erro. Recarregue a aplicação'))
       })
     )
 
